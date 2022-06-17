80076bd00447d0215341d7bed71d0f0e9f5b1ec4
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-06-17 12:16:00 +0300

    userModel created. userExists function added to userController.

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,4 +1,7 @@
 const asyncHandler = require('express-async-handler');
+const bcrypt = require('bcrypt.js'); // to hash the password
+
+const User = require('../models/userModel');
 
 // @desc Register a New User
 // @route /api/users
@@ -6,13 +9,41 @@ const asyncHandler = require('express-async-handler');
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  // simple validation
+  // Validation
   if (!name || !email || !password) {
     res.status(400);
     throw new Error('Please include all fields');
   }
 
-  res.send('Register Route');
+  // Find if user already exists.
+  const userExists = await User.findOne({ email });
+
+  if (userExists) {
+    res.status(400);
+    throw new Error('User already exits.');
+  }
+
+  // Hash Password
+  const salt = await bcrypt.genSalt(10);
+  const hashedPassword = await bcrypt.hash(password, salt);
+
+  // Create user
+  const user = await User.create({
+    name,
+    email,
+    password: hashedPassword,
+  });
+
+  if (user) {
+    res.status(201).json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(400);
+    throw new Error('Invalid user data');
+  }
 });
 
 // @desc Login a  User
