d97add4bf7ecbed8ea2ce66bd49d0b9a109d7f1d
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-06-18 14:55:33 +0300

    auth typo fixed. getMe function created.

--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -6,7 +6,7 @@ const {
   getMe,
 } = require('../controllers/userController');
 
-const { protect } = require('../middleware/auhtMiddleware');
+const { protect } = require('../middleware/authMiddleware');
 
 router.post('/', registerUser);
 
