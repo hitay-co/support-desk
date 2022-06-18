b20819653976996173e017845e1b9adcb6d2c093
hitay-co <107373502+hitay-co@users.noreply.github.com>
2022-06-18 20:41:37 +0300

    Home, Login, and Register components' UI created. react-hook-form package installed and used.

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -1,5 +1,51 @@
+import { useForm } from 'react-hook-form';
+import { FaSignInAlt } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 const Login = () => {
-  return <div>Login</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const { email, password } = data;
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaSignInAlt /> Register
+        </h1>
+        <p>Please login to get support</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-group'>
+            <input
+              type='email'
+              className='form-control'
+              {...register('email', { required: true })}
+              placeholder='Enter your email'
+            />
+            <input
+              type='password'
+              className='form-control'
+              {...register('password', { required: true })}
+              placeholder='Enter your password'
+            />
+          </div>
+          <div>
+            <button className='btn btn-block' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Login;
--- frontend/src/pages/Register.jsx
+++ frontend/src/pages/Register.jsx
@@ -1,5 +1,67 @@
+import { useForm } from 'react-hook-form';
+import { FaUser } from 'react-icons/fa';
+import { toast } from 'react-toastify';
+
 const Register = () => {
-  return <div>Register</div>;
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm();
+
+  const onSubmit = (data) => {
+    const { name, email, password, password2 } = data;
+
+    if (password !== password2) {
+      toast.error('Passwords do not match', { icon: '❗' });
+    }
+  };
+
+  return (
+    <>
+      <section className='heading'>
+        <h1>
+          <FaUser /> Register
+        </h1>
+        <p>Please create an account</p>
+      </section>
+      <section className='form'>
+        <form onSubmit={handleSubmit(onSubmit)}>
+          <div className='form-group'>
+            <input
+              type='text'
+              className='form-control'
+              {...register('name', { required: true })}
+              placeholder='Enter your name'
+            />
+            <input
+              type='email'
+              className='form-control'
+              {...register('email', { required: true })}
+              placeholder='Enter your email'
+            />
+            <input
+              type='password'
+              className='form-control'
+              {...register('password', { required: true })}
+              placeholder='Enter your password'
+            />
+            <input
+              type='password'
+              className='form-control'
+              {...register('password2', { required: true })}
+              placeholder='Confirm your password'
+            />
+          </div>
+          <div>
+            <button className='btn btn-block' type='submit'>
+              Submit
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
 };
 
 export default Register;
